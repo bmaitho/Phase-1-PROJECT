181b75cd15e1fd3cc100e7d9907f3b9ae045f17a
Brian Maitho <Bmaitho@gmail.com>
2024-04-10 16:30:32 +0300

    finished initial CSS styling for the web page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 let url = 'https://www.travel-advisory.info/api/'
 const form = document.getElementById('form')
+ 
+function getCountries(){
+    fetch(https://www.travel-advisory.info/api/   )
+   .then(response => response.json())
+}
